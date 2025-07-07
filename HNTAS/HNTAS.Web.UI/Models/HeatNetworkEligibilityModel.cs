e47b9f789f76370a9e0dbd6ff2c971382cc11fc3
SushreeM-DESNZ <Sushree.Mohanty@energysecurity.gov.uk>
2025-07-07 16:02:51 +0100

    Added HNTAS Eligibility pages

---
+++ HNTAS/HNTAS.Web.UI/Models/HeatNetworkEligibilityModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HNTAS.Web.UI.Models
+{
+    public class HeatNetworkEligibilityModel
+    {
+        public int CurrentStep { get; set; } = 1;
+        public bool? IsRunningHeatNetwork { get; set; } = true;
+        public bool? ServesMoreThan10Dwellings { get; set; } = true;
+        public bool? IsInUK { get; set; } = true;
+        public bool? IsExistingOrPlanned { get; set; } = true;
+        public string ResultMessage { get; set; }
+    }
+}
