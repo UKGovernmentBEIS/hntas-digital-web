fa95940dff22d39b8e93f4f6571dec63e6b1204e
Sivasankar Manokaran <Sivasankar.Manokaran@energysecurity.gov.uk>
2025-07-09 19:37:12 +0100

    Enabled Javascript changes

--- HNTAS/HNTAS.Web.UI/assets/js/main.js
+++ HNTAS/HNTAS.Web.UI/assets/js/main.js
@@ -1,6 +1,8 @@
 ﻿// const GOVUKFrontend = require('./govuk-frontend-all.js');
 import "../scss/main.scss";
 
+import { initAll } from 'govuk-frontend'
+initAll()
 
 // // overriding to allow js on mobile, this must run before the initAll() method  
 // GOVUKFrontend.Tabs.prototype.setupResponsiveChecks = function () {
