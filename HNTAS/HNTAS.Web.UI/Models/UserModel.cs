bfa010812c4796ec29ad04cbaef8ec43d0d13663
Sivasankar Manokaran <Sivasankar.Manokaran@energysecurity.gov.uk>
2025-07-11 12:03:16 +0100

    Update user controller and org contoller to continue the journey

--- HNTAS/HNTAS.Web.UI/Models/UserModel.cs
+++ HNTAS/HNTAS.Web.UI/Models/UserModel.cs
@@ -1,7 +1,13 @@
-﻿namespace HNTAS.Web.UI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HNTAS.Web.UI.Models
 {
     public class UserModel
     {
-        public bool ifRPisRC { get; set; } = true;
+        [Required(ErrorMessage = "Select yes if you are the regulatory contact")]
+        public bool? ifRPisRC { get; set; }
+
+        // Add this property if you want the organisation name to be dynamic
+        public string OrganisationName { get; set; } = string.Empty;
     }
 }
